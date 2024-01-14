64b0499155c9d4100f90f614a2285c3b445ce0ca
vovashabalin <156236554+vovashabalin@users.noreply.github.com>
2024-01-14 08:50:32 +0200

    Update inc.js
    added console.log

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -3,10 +3,16 @@
 let inc_numbers = (format_complete, ...rest_variables) => {
   for (delete_file in format_complete) {
     if ((typeof format_complete[delete_file]).charAt(0).toUpperCase() === 'N') {
-      format_complete[delete_file] = format_complete[delete_file] + 1;
+      format_complete[delete_file] ++;
     }
   }
   return format_complete;
 };
-
+console.log(inc_numbers([1,2,3,4,5]))
+console.log(inc_numbers([10]))
+console.log(inc_numbers(11,16,17))
+console.log(inc_numbers('123654'))
+console.log(inc_numbers(['123654','896']))
+console.log(inc_numbers('Hello'))
+console.log(inc_numbers('True'))
 module.exports = inc_numbers;
